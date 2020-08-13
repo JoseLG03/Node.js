6764ccfe38b7e0c6340c557b0801fb5c99d7080a
Jose <antares069@gmail.com>
2020-08-12 22:02:51 -0500

    funciones y metodos

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-var nombre = "Teodoro";
+function saludo(){
+    var msj = "Hola mundo";
+    return msj;
+}
+
+console.log(saludo());
+
+let calculo ={
+
+    duplicar: function(valor1) {
+        return valor1*2;
+    } ,
+
+    dividir: function(valor1) {
+        return valor1/2;
+    }
+}
+
+console.log( calculo.dividir(10), calculo.duplicar(10));
