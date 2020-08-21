ff8f3dcd21ba75ac6f2e8dc5bb750469779e4ed9
Jose <antares069@gmail.com>
2020-08-20 21:39:32 -0500

    Enrutando proyecto con express

--- expressbase/app.js
+++ expressbase/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var pruebaRouter = require('./routes/prueba');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/prueba', pruebaRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
