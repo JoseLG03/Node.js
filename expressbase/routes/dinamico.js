0725eaf48d2c56d0887f7a548abc667318830c10
Jose <antares069@gmail.com>
2020-08-22 23:22:02 -0500

    next

--- expressbase/routes/dinamico.js
+++ expressbase/routes/dinamico.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(peticion, res,next) {
+router.get('/', function(peticion, res) {
   res.send('Informacion dinamica');
 });
 
-router.get('/:datourl', function(peticion, res,next) {
+router.get('/:datourl', function(peticion, res) {
   res.send("Informacion dinamica: " + peticion.params.datourl);
 });
 
