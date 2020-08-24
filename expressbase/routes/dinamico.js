0228842ca60999c4e1bd238d0f95562cfb3399bb
Jose <antares069@gmail.com>
2020-08-23 20:30:41 -0500

    desplegar valores en PUG

--- expressbase/routes/dinamico.js
+++ expressbase/routes/dinamico.js
@@ -3,11 +3,18 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(peticion, res, next) {
-  res.send('Informacion dinamica');
+  res.render('dinamico');
 });
 
-router.get('/:datourl', function(peticion, res, next) {
-  res.send("Informacion dinamica: " + peticion.params.datourl);
+router.get('/:datoURL/:ID', function(peticion, res, next) {
+  res.render('dinamico', {
+    datos:{
+      titulo:peticion.params.datoURL,
+      ID:peticion.params.ID,
+    }
+  });
 });
+  //res.send("Informacion dinamica: " + peticion.params.datourl);
+
 
 module.exports = router;
