41942a2adecbe571d45f1ed6bdec0b92a97404d9
Jose <antares069@gmail.com>
2020-08-24 22:54:19 -0500

    Mover un archivo

---
+++ metodos_encadenados.js
@@ -0,0 +1,23 @@
+var fs = require('fs'); 
+
+function copiarArchivo(rutaOriginal, nuevaRuta, ArchivoParaCopiar){
+    fs.copyFile(rutaOriginal + '/' + ArchivoParaCopiar , nuevaRuta + '/' + ArchivoParaCopiar, (error)=>{
+        if(error){
+            console.log("Error en el copiado del archivo");
+        }else{
+            eliminarArchivo(rutaOriginal, ArchivoParaCopiar);
+        }
+    })
+}
+
+function eliminarArchivo(rutaOriginal, ArchivoParaCopiar){
+    fs.unlink(rutaOriginal + '/' + ArchivoParaCopiar, (error)=>{
+        if(error){
+            console.log("Error en la funcion de eliminar");
+        }else{
+            console.log("Archivo se movio exitosamente.");
+        }
+    });
+}
+
+copiarArchivo('./archivos/old_files','./archivos/new_files', 'prueba.txt' );
