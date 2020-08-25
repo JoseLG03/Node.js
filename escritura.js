0b5f9f21d20db24f86ed1caaf59879c6880e9698
Jose <antares069@gmail.com>
2020-08-24 21:33:43 -0500

    Escritura de archivo

--- escritura.js
+++ escritura.js
@@ -1,1 +1,9 @@
 var fs = require("fs");
+
+fs.writeFile('./archivos/textos.txt', 'Datos enviados desde Node.js', (error, msj)=>{
+     if(error){
+         console.log("Error en la escritura del archivo.");
+     }else{
+         console.log("Archivo modificado.");
+     }
+});
