8b262a3334daedbc57a5dba06926fd93e6adc124
Jose <antares069@gmail.com>
2020-08-24 21:39:57 -0500

    append

--- escritura.js
+++ escritura.js
@@ -1,9 +1,17 @@
 var fs = require("fs");
-
-fs.writeFile('./archivos/textos.txt', 'Datos enviados desde Node.js', (error, msj)=>{
+/*
+fs.writeFile('./archivos/texto-nuevo.txt', 'Datos enviados desde Node.js.', (error, msj)=>{
      if(error){
          console.log("Error en la escritura del archivo.");
      }else{
          console.log("Archivo modificado.");
      }
+});
+*/
+fs.appendFile('./archivos/texto-nuevo.txt', '      segundo texto agregado posteriormente.', (error, msj)=>{
+    if(error){
+        console.log("Error en la escritura del archivo.");
+    }else{
+        console.log("Archivo actualizado.");
+    }
 });
