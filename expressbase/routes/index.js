cdeb911f89640951dcc6b769412c2ac7a2fa52a1
Jose <antares069@gmail.com>
2020-08-26 20:49:20 -0500

    Desplegar datos JSON en un plantilla

--- expressbase/routes/index.js
+++ expressbase/routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  fs.readFile('./public/json/libros.json', (error, datos)=>{
+
+    if(error){
+      res.write("Error de lectura");
+    }else{
+      res.render('index', { book: JSON.parse(datos) });
+    }
+
+  });
+
 });
 
 module.exports = router;
